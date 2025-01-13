4d90b4a288b3d64438313b6a914dd7d5d676404b
adsk0165 <a.charkaoui@student.avans.nl>
2025-01-13 01:14:38 +0100

    fisrt commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import {FrontpageComponent} from './components/frontpage/frontpage.component'
 import {ActivityMapComponent} from './components/activity/activity-map/activity-map.component'
+import { ActivityCreateComponent } from './components/activity/activity-create/activity-create.component';
 
 export const routes: Routes = [] = [
     // { path: '', redirectTo: '/frontpage', pathMatch: 'full' },
     {path: 'frontpage', component: FrontpageComponent },
     {path: 'activity', component: ActivityMapComponent },
-
+    {path: 'createactivity', component: ActivityCreateComponent}
 ];
--- src/app/components/activity/activity-create/activity-create.component.ts
+++ src/app/components/activity/activity-create/activity-create.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-activity-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './activity-create.component.html',
   styleUrl: './activity-create.component.css'
 })
 export class ActivityCreateComponent {
+  
+  event = {
+    name: '',
+    photo: undefined,
+    cost: 0,
+    description: '',
+    address: '',
+    organizer: '',
+  };
 
+  
+  onFileSelected(event: Event): void {
+    // const input = event.target as HTMLInputElement;
+    // if (input.files && input.files[0]) {
+    //   this.event.photo = input.files[0];
+    // }
+  }
+  
+  createEvent(): void {
+    console.log('Event Created:', this.event);
+
+    // Example: Send the event to a server or local storage
+    // const formData = new FormData();
+    // formData.append('name', this.event.name);
+    // if (this.event.photo) {
+    //   formData.append('photo', this.event.photo);
+    // }
+    // formData.append('cost', this.event.cost.toString());
+    // formData.append('description', this.event.description);
+    // formData.append('address', this.event.address);
+    // formData.append('organizer', this.event.organizer);
+
+    // Send `formData` to a backend using HttpClient
+    alert('Event created successfully!');
+  }
 }
