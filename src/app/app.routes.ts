30735400e5d794f6b57915f17b9d3eed3249fcce
adsk0165 <a.charkaoui@student.avans.nl>
2025-01-13 02:17:23 +0100

    fisrt commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {FrontpageComponent} from './components/frontpage/frontpage.component'
 import {ActivityMapComponent} from './components/activity/activity-map/activity-map.component'
 import { ActivityCreateComponent } from './components/activity/activity-create/activity-create.component';
+import { ActivityDetailsComponent } from './components/activity/activity-details/activity-details.component';
 
 export const routes: Routes = [] = [
     // { path: '', redirectTo: '/frontpage', pathMatch: 'full' },
     {path: 'frontpage', component: FrontpageComponent },
     {path: 'activity', component: ActivityMapComponent },
-    {path: 'createactivity', component: ActivityCreateComponent}
+    {path: 'createactivity', component: ActivityCreateComponent},
+    {path: 'detailsactivity', component: ActivityDetailsComponent}
 ];
