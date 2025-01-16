174ef59a69d8a263a9bf95da3ef952d5312cd022
nix18net <hoiikbenmaurits04@gmail.com>
2025-01-16 11:57:22 +0100

    extra activity fields

---
+++ src/app/components/activity/activity-timeline/activity-timeline.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-timeline',
+  standalone: true,
+  imports: [],
+  templateUrl: './activity-timeline.component.html',
+  styleUrl: './activity-timeline.component.css'
+})
+export class ActivityTimelineComponent {
+
+}
